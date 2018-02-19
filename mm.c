78acbf61524001decab234912952a510134a7127
Dmitriy Shilin <sdadeveloper@gmail.com>
2018-02-19 09:31:15 +0300

    mm: implement basic memory routines

--- mm.c
+++ mm.c
@@ -1,20 +1,168 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "mm.h"
 
-static Header base;
-static Header *freep = NULL;
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-void *mmalloc(const unsigned size)
+Header base;
+Header *freep = NULL;
+
+static void ensure_init_freelist(void);
+static Header *mm_sbrk(unsigned long size);
+static inline unsigned long num_of_blocks(unsigned long size);
+
+void *mmalloc(const unsigned long size)
 {
+	ensure_init_freelist();
+
+	const unsigned long alloc_sz = num_of_blocks(size);
+
+	Header *prevp = freep;
+	Header *p = NULL;
+
+	for (p = prevp->h.ptr; ; prevp = p, p = p->h.ptr) {
+		if (p->h.size >= alloc_sz) {
+			if (p->h.size == alloc_sz)
+				prevp->h.ptr = p->h.ptr;
+			else {
+				p->h.size -= alloc_sz;
+				p += p->h.size;
+				p->h.size = alloc_sz;
+			}
+
+			freep = prevp;
+			return (void *)(p + 1);
+		}
+
+		if (p == freep)
+			if ((p = mm_sbrk(alloc_sz)) == NULL)
+				return NULL;
+	}
+
 	return NULL;
+}
+
+void *mcalloc(unsigned long count, unsigned long size)
+{
+	unsigned long num = count * size;
+	char *ptr = NULL;
+	char *p = NULL;
+
+	if ((p = ptr = mmalloc(num)) == NULL)
+		return NULL;
+
+	for (unsigned long i = 0; i < num; i++)
+		*p++ = 0;
+
+	return ptr;
+}
+
+void *mrealloc(void *ptr, unsigned long size)
+{
+	if (ptr == NULL)
+		return mmalloc(size);
+
+	Header *bp = (Header*)ptr - 1;
+	if (bp == NULL)
+		return NULL;
+
+	const unsigned long alloc_sz = num_of_blocks(size);
+
+	if (bp->h.size >= alloc_sz) {
+		if (bp->h.size == alloc_sz)
+			return (void *)(bp + 1);
+
+		Header *p = bp;
+
+		bp->h.size -= alloc_sz;
+		p += bp->h.size;
+		p->h.size = alloc_sz;
+
+		mfree((void *)(bp + 1));
+		return (void *)(p + 1);
+	}
+
+	void *p = mmalloc(alloc_sz);
+	if (p == NULL)
+		return NULL;
+
+	memcpy(p, ptr, (bp->h.size - 1) * sizeof(Header));
+	mfree((void *)(bp + 1));
+
+	return p;
 }
 
 void mfree(void *ptr)
 {
+	if (ptr == NULL)
+		return;
+
+	Header *bp = (Header *)ptr - 1;
+	if (bp == NULL)
+		return;
+
+	ensure_init_freelist();
+
+	Header *p = NULL;
+	for (p = freep; !(bp > p && bp < p->h.ptr); p = p->h.ptr)
+		if (p >= p->h.ptr && (bp > p || bp < p->h.ptr))
+				break;
+
+	if (bp + bp->h.size == p->h.ptr) {
+		bp->h.size += p->h.ptr->h.size;
+		bp->h.ptr = p->h.ptr->h.ptr;
+	} else
+		bp->h.ptr = p->h.ptr;
+
+	if (p + p->h.size == bp) {
+		p->h.size += bp->h.size;
+		p->h.ptr = bp->h.ptr;
+	} else
+		p->h.ptr = bp;
+
+	freep = p;
 }
 
-Header *_mm(unsigned size)
+void mfree_arbitrary(void *ptr, unsigned long size)
 {
-	return NULL;
+	if (ptr == NULL || size == 0 || size < sizeof(Header))
+		return;
+
+	Header *hp = (Header *)ptr;
+	hp->h.size = size / sizeof(Header);
+	mfree((void *)(hp + 1));
+}
+
+static Header *mm_sbrk(unsigned long size)
+{
+	if (size < BLOCKSIZ)
+		size = BLOCKSIZ;
+
+	Header *p = NULL;
+	char *ptr = NULL;
+
+	if ((ptr = sbrk(size * sizeof(Header))) == (char *)(-1))
+		return NULL;
+
+	p = (Header *)ptr;
+	p->h.size = size;
+
+	mfree((void *)(p + 1));
+	return freep;
+}
+
+static void ensure_init_freelist(void)
+{
+	if (freep == NULL) {
+		base.h.size = 0;
+		base.h.ptr = &base;
+		freep = base.h.ptr;
+	}
+}
+
+static inline unsigned long num_of_blocks(unsigned long size)
+{
+	return (size + sizeof(Header) - 1) / sizeof(Header) + 1;
 }
--- test.c
+++ test.c
@@ -6,7 +6,66 @@
 
 int main()
 {
-	void *ptr = mmalloc(2);
-	assert(ptr != NULL);
-	mfree(ptr);
+	{
+		int *ptr = (int *)mmalloc(sizeof(int)*10);
+		assert(ptr != NULL);
+		mfree(ptr);
+	}
+	{
+		int *ptr = (int *)mmalloc(sizeof(int)*10);
+
+		*ptr = 100;
+		assert(*ptr == 100);
+
+		mfree(ptr);
+	}
+	{
+		mfree(NULL);
+	}
+	{
+		int *ptr = (int *)mcalloc(10, sizeof(int));
+
+		assert(ptr != NULL);
+		assert(*ptr == 0);
+
+		mfree(ptr);
+	}
+	{
+		char pool[BUFSIZ];
+		char *ptr = pool;
+		mfree_arbitrary((void *)ptr, BUFSIZ);
+
+		int *p = (int *)mmalloc(10 * sizeof(int));
+		assert(p != NULL);
+		*p = 1000;
+		assert(*p == 1000);
+	}
+	{
+		int *ptr = (int *)mrealloc(NULL, 10 * sizeof(int));
+		assert(ptr != NULL);
+	}
+	{
+		int *ptr = (int *)mmalloc(sizeof(int));
+		*ptr = 100;
+
+		ptr = (int *)mrealloc(ptr, 200 * sizeof(int));
+		*(ptr + 1) = 200;
+
+		assert(*ptr == 100);
+		assert(*(ptr + 1) == 200);
+
+		mfree(ptr);
+	}
+	{
+		int *ptr = (int *)mmalloc(20 * sizeof(int));
+		*ptr = 100;
+		*(ptr + 1) = 200;
+
+		ptr = (int *)mrealloc(ptr, sizeof(int));
+		*ptr = 300;
+
+		assert(*ptr == 300);
+
+		mfree(ptr);
+	}
 }
